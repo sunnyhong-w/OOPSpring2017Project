b272ec0e9b32004c128677cad070aa960908ced7
Po-Hsu,Yang <kuroneko14123@gmail.com>
2017-02-15 16:32:36 +0800

    + add Vector2 and Vector2I operator overload

--- Source/enginelib.cpp
+++ Source/enginelib.cpp
@@ -31,9 +31,53 @@ Vector2::Vector2(float x, float y)
     this->y = y;
 }
 
+Vector2 Vector2::operator-(Vector2 subtraction)
+{
+    return Vector2(this->x - subtraction.x, this->y - subtraction.y);
+}
+
+Vector2 Vector2::operator+(Vector2 adder)
+{
+    return Vector2(this->x + adder.x, this->y + adder.y);
+}
+
+Vector2 operator*(Vector2 multiplied, Vector2 multiplier)
+{
+    return Vector2(multiplied.x * multiplier.x, multiplied.y * multiplier.y);
+}
+
+Vector2 operator*(Vector2 multiplied, double multiplier)
+{
+    return Vector2(multiplied.x * multiplier, multiplied.y * multiplier);
+}
+
+Vector2 operator*(double multiplied, Vector2 multiplier)
+{
+    return Vector2(multiplied * multiplier.x, multiplied * multiplier.y);
+}
+
+Vector2 operator/(Vector2 multiplied, Vector2 multiplier)
+{
+    return Vector2(multiplied.x / multiplier.x, multiplied.y / multiplier.y);
+}
+Vector2 operator/(Vector2 multiplied, double multiplier)
+{
+    return Vector2(multiplied.x / multiplier, multiplied.y / multiplier);
+}
+
+Vector2 operator/(double multiplied, Vector2 multiplier)
+{
+    return Vector2(multiplied / multiplier.x, multiplied / multiplier.y);
+}
+
 Vector2I Vector2::GetV2I()
 {
     return Vector2I(this);
+}
+
+string Vector2::toString()
+{
+    return '(' + to_string(this->x) + ',' + to_string(this->y) + ')';
 }
 
 bool Vector2::isNull()
@@ -58,5 +102,46 @@ Vector2I::Vector2I(Vector2* v2)
     this->x = (int)v2->x;
     this->y = (int)v2->y;
 }
+
+Vector2I Vector2I::operator-(Vector2I subtraction)
+{
+    return Vector2I(this->x - subtraction.x, this->y - subtraction.y);
+}
+
+Vector2I Vector2I::operator+(Vector2 adder)
+{
+    return Vector2I(this->x + adder.x, this->y + adder.y);
+}
+
+Vector2I operator*(Vector2I multiplied, Vector2I multiplier)
+{
+    return Vector2I(multiplied.x * multiplier.x, multiplied.y * multiplier.y);
+}
+
+Vector2I operator*(Vector2I multiplied, int multiplier)
+{
+    return Vector2I(multiplied.x * multiplier, multiplied.y * multiplier);
+}
+
+Vector2I operator*(int multiplied, Vector2I multiplier)
+{
+    return Vector2I(multiplied * multiplier.x, multiplied * multiplier.y);
+}
+
+Vector2I operator/(Vector2I multiplied, Vector2I multiplier)
+{
+    return Vector2I(multiplied.x / multiplier.x, multiplied.y / multiplier.y);
+}
+
+Vector2I operator/(Vector2I multiplied, int multiplier)
+{
+    return Vector2I(multiplied.x / multiplier, multiplied.y / multiplier);
+}
+
+Vector2I operator/(int multiplied, Vector2I multiplier)
+{
+    return Vector2I(multiplied / multiplier.x, multiplied / multiplier.y);
+}
+
 
 }
--- Source/enginelib.h
+++ Source/enginelib.h
@@ -6,12 +6,11 @@ HISTORY :
 */
 
 #pragma once
-
+#include <string>
+using namespace std;
 namespace game_engine
 {
-
 struct Vector2I;
-
 struct Vector2
 {
     public:
@@ -28,7 +27,18 @@ struct Vector2
         const static Vector2 one;
         const static Vector2 zero;
         const static Vector2 null;
+        Vector2 operator -(Vector2 subtraction);
+        Vector2 operator +(Vector2 adder);
+        friend Vector2 operator *(Vector2 multiplied, Vector2 multiplier);
+        friend Vector2 operator *(Vector2 multiplied, double multiplier);
+        friend Vector2 operator *(double multiplied, Vector2 multiplier);
+        friend Vector2 operator /(Vector2 multiplied, Vector2 multiplier);
+        friend Vector2 operator /(Vector2 multiplied, double multiplier);
+        friend Vector2 operator /(double multiplied, Vector2 multiplier);
+
+        //Vector2I operator =(Vector2 equ);
         Vector2I GetV2I();
+        string toString();
         bool isNull();
     private:
         bool nullVector = false;
@@ -40,8 +50,15 @@ struct Vector2I
         Vector2I();
         Vector2I(int x, int y);
         Vector2I(Vector2* v2);
+        Vector2I operator -(Vector2I subtraction);
+        Vector2I operator +(Vector2 adder);
+        friend Vector2I operator *(Vector2I multiplied, Vector2I multiplier);
+        friend Vector2I operator *(Vector2I multiplied, int multiplier);
+        friend Vector2I operator *(int multiplied, Vector2I multiplier);
+        friend Vector2I operator /(Vector2I multiplied, Vector2I multiplier);
+        friend Vector2I operator /(Vector2I multiplied, int multiplier);
+        friend Vector2I operator /(int multiplied, Vector2I multiplier);
         int x;
         int y;
 };
-
 }
