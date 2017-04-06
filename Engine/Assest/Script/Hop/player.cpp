8a09df2b35665f2005c2ce6a3bb7176f17219f1b
Sunny Hong <hongmusic0204@gmail.com>
2017-04-06 22:34:48 +0800

    + add red room layout

--- Engine/Assest/Script/Hop/player.cpp
+++ Engine/Assest/Script/Hop/player.cpp
@@ -83,10 +83,10 @@ void Player::Update()
 		//GameScene::CameraPosition() = (this->transform->GetPostion() - size).GetV2I();
 	}
 
-    if (Input::GetKeyDown(VK_F5))
-        GameScene::NowScene()->LoadScene("Main");
 	Vector2 size = Vector2((float)SIZE_X / 2, (float)SIZE_Y / 2);
-	GameScene::CameraPosition() = (this->transform->GetPostion() - size).GetV2I();
+	
+    if (!Input::GetKeyPressing(VK_F5))
+        GameScene::CameraPosition() = (this->transform->GetPostion() - size).GetV2I();
 
 	bounce = false;
 }
