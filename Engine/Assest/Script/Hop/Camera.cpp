c5732d5e40500d391f980653c973c1e605b0bacd
Sunny Hong <hongmusic0204@gmail.com>
2017-04-09 03:05:36 +0800

    * fix camera position can't see the bottom tile * fix camera pos so player is in middle

--- Engine/Assest/Script/Hop/Camera.cpp
+++ Engine/Assest/Script/Hop/Camera.cpp
@@ -10,14 +10,37 @@ void Camera::Start()
 
 void Camera::LateUpdate()
 {
-	if(player == nullptr)
-		player = GameObject::findGameObjectByName("Player");
-	else
-	{
-		GameScene::CameraPosition() = this->gameObject->spriteRenderer->GetRealRenderPostion();
-		playerPos = player->transform->GetPostion();
-		Vector2::Damp(this->transform->GetPostion(), playerPos, velocity, 0.0001f, 1, 0.5f);
-		
-		this->gameObject->rigidbody->velocity = (velocity * Time::deltaTime * 8).floorSpecial();
-	}
+    if (Input::GetKeyPressing(VK_F5))
+    {
+        if (Input::GetKeyPressing(VK_NUMPAD8))
+            GameScene::CameraPosition() = GameScene::CameraPosition() + Vector2I::up * 10;
+        if (Input::GetKeyPressing(VK_NUMPAD6))
+            GameScene::CameraPosition() = GameScene::CameraPosition() + Vector2I::right * 10;
+        if (Input::GetKeyPressing(VK_NUMPAD4))
+            GameScene::CameraPosition() = GameScene::CameraPosition() + Vector2I::left * 10;
+        if (Input::GetKeyPressing(VK_NUMPAD2))
+            GameScene::CameraPosition() = GameScene::CameraPosition() + Vector2I::down * 10;
+        if (Input::GetKeyDown(VK_NUMPAD5))
+            GameScene::CameraPosition() = (this->transform->GetPostion() - Vector2(((float)SIZE_X / 2), ((float)SIZE_Y / 2))).GetV2I();
+    }
+    else
+    {
+        if (player == nullptr)
+        {
+            player = GameObject::findGameObjectByName("Player");
+            if (player != nullptr)
+            {
+                this->transform->SetPosition(player->transform->GetWorldPosition());
+                GameScene::CameraPosition() = this->gameObject->spriteRenderer->GetRealRenderPostion();
+                this->gameObject->rigidbody->velocity = Vector2::zero;
+            }
+        }
+        else
+        {
+            GameScene::CameraPosition() = this->gameObject->spriteRenderer->GetRealRenderPostion();
+            playerPos = player->transform->GetWorldPosition() + (player->collider->collisionInfo.size / 2);
+            Vector2::Damp(this->transform->GetWorldPosition(), playerPos, velocity, 0.0001f, 1, 0.5f);
+            this->gameObject->rigidbody->velocity = (velocity * Time::deltaTime * 8).floorSpecial();
+        }
+    }
 }
