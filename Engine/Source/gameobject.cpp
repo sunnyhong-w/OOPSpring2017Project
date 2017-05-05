683aa1ee43b5a76a58f59769ffb29045e85b5318
Po-Hsu,Yang <kuroneko14123@gmail.com>
2017-05-05 11:30:33 +0800

    *fix Destroy again

--- Engine/Source/gameobject.cpp
+++ Engine/Source/gameobject.cpp
@@ -314,9 +314,8 @@ void Destroy(GameObject* gobj)
 {
     gobj->destoryFlag = true;
     gobj->transform->SetParent(nullptr);
+	gobj->SetEnable(false);
 
-	if (gobj->spriteRenderer != nullptr)
-		gobj->spriteRenderer->SetEnable(false);
 
     auto childList = gobj->transform->GetChild();
     for (auto childTransform : childList)
