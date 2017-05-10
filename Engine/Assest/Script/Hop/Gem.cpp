dd528be9f4d7aac353874af9364c4772f0924f77
Po-Hsu,Yang <kuroneko14123@gmail.com>
2017-05-11 01:16:08 +0800

    +add cool gem collect effect

--- Engine/Assest/Script/Hop/Gem.cpp
+++ Engine/Assest/Script/Hop/Gem.cpp
@@ -5,7 +5,7 @@
 void Gem::Start()
 {	
     name = this->gameObject->GetName();
-
+	currentVelocity = Vector2::zero;
 	if (name == "RedGem")
 	{	
 		this->gameObject->spriteRenderer->SetEnable(false);
@@ -23,17 +23,34 @@ void Gem::Start()
 			}
 		}
 	}
+	radius = Vector2(30.0f, 30.0f);
+}
+
+void Gem::Update()
+{
+	if (GameStatus::status[to_string((int)StatusName::Gem)][name].get<int>() == 2) {
+		this->gameObject->spriteRenderer->SetAnchorRaito(Vector2(0.5, 0.5));
+		Vector2 minusedScale = (this->transform->scale - Vector2::one*0.01);
+		GameObject * player = GameObject::findGameObjectByName("Player");
+		Vector2 targetPos = player->transform->GetWorldPosition() + (player->spriteRenderer->GetSize().GetV2()/2);
+		
+		this->transform->scale = Vector2( clamp(minusedScale.x,0,FLT_MAX),clamp(minusedScale.y,0,FLT_MAX));
+		this->transform->SetWorldPosition(targetPos + radius *minusedScale * Vector2(sinf((minusedScale.x + 0.25) * 2 * 3.1415926 * 1.2), cosf((minusedScale.y + 0.25) * 2 * 3.1415926 *1.2)));
+		if (this->transform->scale == Vector2::zero)
+		{
+			Destroy(this->gameObject);
+		}
+	}
 }
 
 void Gem::OnCollisionEnter(Collider * c)
 {
 	GameStatus::status[to_string((int)StatusName::Gem)][name] = 2;
 	GameStatus::SaveFile();
-
+	this->gameObject->collider->SetEnable(false);
     if (name == "RedGem")
         game_framework::CGame::Instance()->SendEvent(WM_CLOSE, 0, 0, "Hop");
-
-	Destroy(this->gameObject);
+	
 }
 
 void Gem::OnRecivedBroadcast(BroadcastMessageData bmd)
